464d1d1c3033c83d15037a0415f5650528510047
Geraldo Luiz <geraldobl58@gmail.com>
2026-07-22 18:46:18 -0300

    update OrderServiceImpl to use new stock service endpoint and change server port to 8082

--- order-service/src/main/java/com/ecomerce/order_service/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/ecomerce/order_service/service/impl/OrderServiceImpl.java
@@ -37,7 +37,7 @@ public class OrderServiceImpl implements OrderService {
             Integer quantity = item.getQuantity();
 
             Boolean isInStock = webClientBuilder.build().get()
-                    .uri("http://localhost:8081/api/v1/orders" + sku, uriBuilder -> uriBuilder
+                    .uri("http://localhost:8082/api/v1/stock/" + sku, uriBuilder -> uriBuilder
                             .queryParam("quantity", quantity).build())
                     .retrieve()
                     .bodyToMono(Boolean.class)
